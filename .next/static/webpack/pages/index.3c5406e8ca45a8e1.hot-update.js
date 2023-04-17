d49b856f888c7c395aa883bb425e75cdca1d61fc
Antônio Neto <acm.neto1999@gmail.com>
2023-04-17 17:09:28 -0300

    :tada: initial commit

---
+++ .next/static/webpack/pages/index.3c5406e8ca45a8e1.hot-update.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("pages/index",{
+
+/***/ "./config.json":
+/*!*********************!*\
+  !*** ./config.json ***!
+  \*********************/
+/***/ (function(module, __unused_webpack_exports, __webpack_require__) {
+
+module.exports = JSON.parse('{"name":"Antônio Neto","github":"traozin","job":"Intern Full Stack Developer","playlists":{"jogos":[{"title":"[PVP] Eu só queria cag4r e me chamaram pra roubar um fortuna... | Sea of Thieves       ","url":"https://www.youtube.com/watch?v=Gm6g7r6Uu8c","thumbnail":"thumb"}],"carros":[{"title":"TEST DRIVE PEUGEOT 208 ELÉTRICO 2023","url":"https://www.youtube.com/watch?v=5LxMb1W-lTc","thumbnail":"https://i.ytimg.com/vi/5LxMb1W-lTc/hq720.jpg?sqp=-oaymwE2CNAFEJQDSFXyq4qpAygIARUAAIhCGAFwAcABBvABAfgB_gmAAtAFigIMCAAQARhIIFgoZTAP&rs=AOn4CLBtrHCsrrYq_Mn8iJBRQbUxHIC7Rw"}]}}');
+
+/***/ })
+
+});
